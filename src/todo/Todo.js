bb5e58bf4bada3bb205bf86c406929036d54c2b4
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-18 10:13:09 +0800

    update

---
+++ src/todo/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoList from '../todo-list/TodoList'
+
+const Todo = () => {
+    return (
+        <div>
+            <h3>To Do List</h3>
+            <TodoList />
+        </div>
+    )
+}
+
+export default Todo
