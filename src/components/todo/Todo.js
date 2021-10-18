68d09e154b461f86fc64cae4e7280904f3de3df6
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-18 12:55:56 +0800

    updt

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoList from '../todo-list/TodoList'
+import TodoCreate from '../todo-create/TodoCreate'
 
 const Todo = () => {
     const todos = [
@@ -7,9 +8,16 @@ const Todo = () => {
         { id:2, title:'Sleep' },
         { id:3, title:'Code' },
     ]
+
+    const eventCreateToDo = (todo) =>{
+        todos.push(todo)
+        console.log(todos)
+    }
+
     return (
         <div>
             <h3>To Do List</h3>
+            <TodoCreate onCreateTodo={ eventCreateToDo }/>
             <TodoList dataTodos= {todos}/>
         </div>
     )
